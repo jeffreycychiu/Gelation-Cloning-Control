783dd37aa2b83f0f3ad8062449029dd476f4d67a
Jeffrey Chiu <jeffreycychiu@gmail.com>
2016-10-03 13:22:14 -0700

    Added the settings for Arroyo serial port.

--- Gelation Cloning Control/MainWindow.xaml.cs
+++ Gelation Cloning Control/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO.Ports;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,9 +22,27 @@ namespace Gelation_Cloning_Control
     /// </summary>
     public partial class MainWindow : MetroWindow
     {
+        SerialPort serialPortArroyo = new SerialPort();
+
+
         public MainWindow()
         {
             InitializeComponent();
+
+            //Set the serial port object to the Arroyo Driver fixed defaults
+            serialPortArroyo.BaudRate = 38400;
+            serialPortArroyo.Parity = Parity.None;
+            serialPortArroyo.DataBits = 8;
+            serialPortArroyo.StopBits = StopBits.One;
+            serialPortArroyo.Handshake = Handshake.None; //Flow control = none
+        }
+
+        //Fill the combo box with the names of the avaliable serial ports
+        private void cmbBoxSerialPort_DropDownOpened(object sender, EventArgs e)
+        {
+            
         }
     }
+
 }
+
